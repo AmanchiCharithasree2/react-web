97c4336b29bd2a62874ad9414e8d70f33929dcbc
= <amanchicharithasree@gmail.com>
2024-07-05 12:43:04 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/Home/Home';
 import NewsComponent from './components/news/NewsComponent';
 import Video from './components/video/Video';
-import {BrowserRouter,Route,Routes} from "react-router-dom";
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
